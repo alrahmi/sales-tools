afa427303b39ebea32804033e1156e8ac8cb4f3b
Banano <banan@gmail.com>
2016-06-05 12:02:54 +0300

    format code and add name ahmed

--- salestools/src/com/tutorials/Main.java
+++ salestools/src/com/tutorials/Main.java
@@ -8,8 +8,8 @@ public class Main {
 	}
 
 	private static void displayGreeting() {
-System.out.println("Hello happy sales people");
-	System.out.println("this app shows sales data");
-	System.out.println("ali");
+		System.out.println("Hello happy sales people");
+		System.out.println("this app shows sales data");
+		System.out.println("ali+ahmed");
 	}
 }
